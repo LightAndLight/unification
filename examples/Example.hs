d7a9244d108c69229676b6a50823e9b9023e496e
Isaac Elliott <isaace71295@gmail.com>
2018-01-31 21:23:30 +1000

    Fixed example

--- examples/Example.hs
+++ examples/Example.hs
@@ -3,15 +3,20 @@
 {-# language DeriveTraversable #-}
 {-# language MultiParamTypeClasses #-}
 {-# language LambdaCase #-}
+{-# language TemplateHaskell #-}
 module Example where
 
 import Control.Lens
 import Control.Lens.Plated1
 import Control.Monad
 import Control.Monad.Unify
+import Data.Deriving
 
 data Ty a = TyArr (Ty a) (Ty a) | TyVar a
   deriving (Eq, Show, Ord, Functor, Foldable, Traversable)
+deriveEq1 ''Ty
+deriveOrd1 ''Ty
+deriveShow1 ''Ty
 
 instance AsVar Ty where
   _Var = prism' TyVar (\case; TyVar a -> Just a; _ -> Nothing)
@@ -22,7 +27,6 @@ instance Plated1 Ty where
 
 instance Unifiable Ty where
   toplevelEqual TyArr{} TyArr{} = True
-  toplevelEqual (TyVar a) (TyVar b) = a == b
   toplevelEqual _ _ = False
 
 instance HasAnnotation Ty () where
@@ -33,11 +37,11 @@ instance Monad Ty where
   TyVar a      >>= f = f a
   TyArr a b >>= f = TyArr (a >>= f) (b >>= f)
 
-do_it :: Either (UnificationError (Ty (Either UVar String)) ()) (Maybe (Ty String))
+do_it :: Either (UnificationError Ty String ()) (Maybe (Ty String))
 do_it = runUnifyT $ do
   a <- fresh
   b <- fresh
-  let t1 = TyArr (TyVar $ Right "a") (TyArr (TyVar $ Left b) $ TyVar $ Left a)
-  let t2 = TyArr (TyVar $ Left a) (TyArr (TyVar $ Left a) $ TyVar $ Right "a")
+  let t1 = view uterm $ TyArr (TyVar $ Right "a") (TyArr (TyVar $ Left b) $ TyVar $ Left a)
+  let t2 = view uterm $ TyArr (TyVar $ Left a) (TyArr (TyVar $ Left a) $ TyVar $ Right "a")
   unify t1 t2
   freeze t1
